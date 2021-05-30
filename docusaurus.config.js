d5dbc6532500414a491b0d79a199c4d77b9855ad
FabianaFerreira <fabiana.ferreira@poli.ufrj.br>
2021-05-30 15:15:00 -0300

    Changing style and adding content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ module.exports = {
   projectName: 'Journal', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Journal',
+      title: 'Fabiana Fonseca',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -21,11 +21,11 @@ module.exports = {
           type: 'doc',
           docId: 'intro',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Studies',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/FabianaFerreira/',
           label: 'GitHub',
           position: 'right',
         },
@@ -33,48 +33,7 @@ module.exports = {
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Fabiana Fonseca, Inc. Built with Docusaurus.`,
     },
   },
   i18n: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,63 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={styles.header}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
+        <h1 className={styles.title}>{siteConfig.title}</h1>
+        <p>
+          I'm an Electronic and Computer Engineer graduated from the Federal
+          University of Rio de Janeiro (UFRJ), RJ, Brazil. Currently working on
+          VTEX as a Software Engineer. Has previous experience with Computer
+          Vision and Data Visualization. I am a person eager to learn new
+          things, especially those related to Software Engineering and Data
+          Science. Besides that, I love to study different languages, their
+          literature and get to know more about linguistics facts.
+        </p>
+        <div className={styles.buttons}></div>
       </div>
     </header>
   );
 }
 
+const Section = ({ img, title, link }) => (
+  <div className={styles.section}>
+    <img className={styles.sectionImg} src={`../static/home/${img}.png`} />
+    <Link className={clsx("button button--primary", styles.buttons)} to={link}>
+      {title}
+    </Link>
+  </div>
+);
+
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      title={`${siteConfig.title}`}
+      description="Software Engineer from Rio de Janeiro, Brazil"
+    >
+      <div className={styles.homeContainer}>
+        <HomepageHeader />
+        <div className={styles.sections}>
+          <Section
+            img={"ginger-cat-759"}
+            title="Curriculum Vitae"
+            link="/resume"
+          />
+          <Section img={"ginger-cat-750"} title="Articles" link="/docs" />
+          <Section
+            img={"ginger-cat-cat-is-reading"}
+            title="Journal"
+            link="/blog"
+          />
+        </div>
+      </div>
     </Layout>
   );
 }
